6e5e1d3d53d952e9fd13a7e7b146dc0091e9b287
peidli <stefanpeidli@gmail.com>
2022-10-31 09:47:00 +0100

    Add tests for CI

---
+++ tests/main_test.py
@@ -0,0 +1,7 @@
+import scvelo as scv
+from mousipy import translate
+
+adata = scv.datasets.pancreas()  # mouse scRNA-seq dataset
+humanized_adata = translate(adata)
+assert humanized_adata.n_obs == adata.n_obs, "We lost cells during mapping, which should not happen!"
+assert humanized_adata.n_vars > 10000, "Very few genes (less than 10k) could be mapped! Expecting around 17.5k."
