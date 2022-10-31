15470af5a1cf97a156eeb919b7eef8a5862b2e48
peidli <stefanpeidli@gmail.com>
2022-10-31 10:23:34 +0100

    Test functions must contain 'test' in function name.

--- src/tests/main_test.py
+++ src/tests/main_test.py
@@ -1,7 +1,8 @@
 import scvelo as scv
 from mousipy import translate
 
-adata = scv.datasets.pancreas()  # mouse scRNA-seq dataset
-humanized_adata = translate(adata)
-assert humanized_adata.n_obs == adata.n_obs, "We lost cells during mapping, which should not happen!"
-assert humanized_adata.n_vars > 10000, "Very few genes (less than 10k) could be mapped! Expecting around 17.5k."
+def test_pancreas():
+    adata = scv.datasets.pancreas()  # mouse scRNA-seq dataset
+    humanized_adata = translate(adata)
+    assert humanized_adata.n_obs == adata.n_obs, "We lost cells during mapping, which should not happen!"
+    assert humanized_adata.n_vars > 10000, "Very few genes (less than 10k) could be mapped! Expecting around 17.5k."
